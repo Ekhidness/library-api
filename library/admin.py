255f33fdb35a86e957e7b2a93b13fd86df68795d
Ekhidna <ekhidna@disroot.org>
2025-10-22 18:54:45 +0700

    Настроена админка и миграции

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Author, Book
 
-# Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    search_fields = ('first_name', 'last_name')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year', 'category', 'publisher')
+    list_filter = ('category', 'genre', 'publication_year')
+    search_fields = ('title', 'author__first_name', 'author__last_name', 'publisher')
