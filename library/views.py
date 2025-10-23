66ddc084bd4d7eac10540bd6b1a781d0fc618b7f
ekhidna <ekhidna@disroot.org>
2025-10-23 10:23:57 +0700

    Добавлены основные представления

--- library/views.py
+++ library/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.db.models import Q
+from rest_framework import generics, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
 
-# Create your views here.
+class AuthorListView(generics.ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.AllowAny]
+
+class AuthorDetailView(generics.RetrieveAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.AllowAny]
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.select_related('author')
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.select_related('author')
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAdminUser]
